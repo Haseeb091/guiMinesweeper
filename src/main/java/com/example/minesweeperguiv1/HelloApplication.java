0d4878846682bd30116e14af0e870f641a425ed6
Haseeb <haseeb091097@gmail.com>
2022-11-16 15:09:55 +0000

    added row and col input page for gui

--- src/main/java/com/example/minesweeperguiv1/HelloApplication.java
+++ src/main/java/com/example/minesweeperguiv1/HelloApplication.java
@@ -1,8 +1,16 @@
 package com.example.minesweeperguiv1;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,6 +21,68 @@ public class HelloApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void rowColInputPage(Stage stage){
+        TextField row=new TextField();
+        TextField col=new TextField();
+        Button play=new Button("play");
+        play.setOnMouseClicked(new EventHandler<MouseEvent>() {
+
+            @Override
+            public void handle(MouseEvent event) {
+                MouseButton button = event.getButton();
+
+                if(button==MouseButton.PRIMARY){// left click
+                    try {
+                        int rowValue=Integer.parseInt(row.getText());
+                        int colValue=Integer.parseInt(col.getText());
+                        if((rowValue < 5 || rowValue > 30)){
+
+                            throw new Exception("Not_in_range");
+                        }
+                        if((colValue < 5 || colValue > 30)){
+
+                            throw new Exception("Not_in_range");
+                        }
+                       //start game page
+
+                    }catch (NumberFormatException e){
+                        Alert notAnumb = new Alert(Alert.AlertType.WARNING);
+                        notAnumb.setContentText("please enter valid integers");
+
+                        notAnumb.show();
+
+
+                    }
+                    catch (Exception e){
+
+                        if(e.toString().contains("Not_in_range")){
+                            Alert notInRange = new Alert(Alert.AlertType.WARNING);
+                            notInRange.setContentText("please enter valid integers between 5 -29");
+
+                            notInRange.show();
+                        }
+
+                    }
+
+
+                }
+
+            }
+        });
+
+        GridPane gridPane = new GridPane();
+
+        gridPane.add(new Label("row"),1,1,1,1);
+        gridPane.add(row,2,1,1,1);
+
+        gridPane.add(new Label("col"),1,2,1,1);
+        gridPane.add(col,2,2,1,1);
+        gridPane.add(play,3,3,1,1);
+        Scene scene =new Scene(gridPane, 240, 100);
+
         stage.setScene(scene);
         stage.show();
     }
